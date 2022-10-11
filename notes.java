96c70ca9dd3310d70c0fbf85a7a09da4bf820888
Mehmet <75950443+PloKoon68@users.noreply.github.com>
2022-10-11 15:53:30 +0300

    Update notes.java

--- notes.java
+++ notes.java
@@ -11,12 +11,20 @@ import javax.swing.*;
 public class JavaApplication1 {
 
     public static void main(String[] args) {
-     
+         /*
+        JFrame myWindow;
+
+        myWindow = new JFrame();
+
+        myWindow.setSize(300, 200);
+        myWindow.setTitle("My First Java Program");
+        myWindow.setVisible(true);
+        myWindow.getContentPane().setBackground(Color.darkGray);
+        
+        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        */ 
         
         
         
     }
-
-   
-
 }
