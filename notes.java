82b6d2384aa630ab24ba1e6049b06404a5707ce5
Mehmet <75950443+PloKoon68@users.noreply.github.com>
2022-10-11 16:02:48 +0300

    Update notes.java

--- notes.java
+++ notes.java
@@ -11,7 +11,8 @@ import javax.swing.*;
 public class JavaApplication1 {
 
     public static void main(String[] args) {
-         /*
+        //----------------- Simple JFrame window
+        /*
         JFrame myWindow;
 
         myWindow = new JFrame();
@@ -26,5 +27,72 @@ public class JavaApplication1 {
         
         
         
+        
+        //-----------------  Displaying string from the JOptionPanel
+        /*
+        String  name, first, middle, last,
+                space, monogram;
+        
+        space = " ";
+
+        //Input the full name
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.useDelimiter(System.getProperty("line.separator"));
+        
+        //System.out.print("Enter your full name (first, middle, last):");
+        
+        //name = scanner.next( );
+        name = JOptionPane.showInputDialog(null, "Enter your full name (first, middle, last):");
+
+
+        //Extract first, middle, and last names
+        first  = name.substring(0, name.indexOf(space));
+        name   = name.substring(name.indexOf(space)+1, name.length());
+
+        middle = name.substring(0, name.indexOf(space));
+        last   = name.substring(name.indexOf(space)+1, name.length());
+
+        //Verify the substring operations
+//        System.out.println("First:"  + first);
+//        System.out.println("Middle:" + middle);
+//        System.out.println("Last:"   + last);
+
+        //Compute the monogram
+        monogram = first.substring(0, 1)
+                    + middle.substring(0, 1)
+                        + last.substring(0, 1);
+        JOptionPane.showMessageDialog(null, monogram);
+        //Output the result
+        System.out.println("Your monogram is " + monogram);
+        */
+        
+       
+        
+        
+        //---------------Same thing but with arranging the the JOptionPanel(text color, background color) using the UIManager class 
+        /*
+        UIManager um = new UIManager();
+        um.put("OptionPane.messageForeground", Color.red);
+        um.put("Panel.background", Color.green);
+       
+        String name = JOptionPane.showInputDialog(null, "Enter your full name (first, middle, last):");
+       
+        JOptionPane.showMessageDialog(null, "Your name is " + name);
+        */
     }
 }
+
+
+
+
+
+
+
+
+
+//notes:
+/*
+    - You should know which class is from which package
+
+*/
