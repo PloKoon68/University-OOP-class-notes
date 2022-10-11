d646343da5269ff33ebfec822dfc437413f95a58
Mehmet <75950443+PloKoon68@users.noreply.github.com>
2022-10-11 15:52:17 +0300

    Create notes.java

---
+++ notes.java
@@ -0,0 +1,22 @@
+
+package javaapplication1;
+
+
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+     
+        
+        
+        
+    }
+
+   
+
+}
